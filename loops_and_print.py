a899a57ab432263e0930fdd0cac6058321116ee5
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-05-23 23:57:20 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,21 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(la_lista):
+    lista_numerada = []
+    contador = 0
+    for item in la_lista:
+        if item != "":
+            elemento_nuevo = f"{contador}, {item}"
+            lista_numerada.append(elemento_nuevo)
+            contador += 1
+    return lista_numerada
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(la_list):
+    lista_invertida = []
+    contador = 0
+    for item in la_lista:
+        if item != "":
+            palabra_invertida = item [:: -1]
+            elemento_nuevo = f"{contador}, {palabra_invertida}"
+            lista_invertida.append(elemento_nuevo)
+            contador +=1
+    return lista_invertida
